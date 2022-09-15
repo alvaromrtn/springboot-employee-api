fb5e33120198d77659bca4f18f29d8780a2883a8
alvaromrtn <alvaromrtn@gmail.com>
2022-09-15 02:07:37 +0200

    Swagger

--- rest-user-service-application/src/main/java/com/axpe/exercices/Application.java
+++ rest-user-service-application/src/main/java/com/axpe/exercices/Application.java
@@ -12,6 +12,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 /**
  * Clase para arrancar el Webnode como una aplicación Spring-boot
  * 
@@ -20,6 +25,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
  */
 @SpringBootApplication
 @EnableConfigurationProperties
+@OpenAPIDefinition(	info = @Info(	title = "API EMPLOYEE - AXPE",
+									version = "Versión 2.0.0",
+									description = "Application within the technological framework of API Methodology.",
+									contact = @Contact(name = "Álvaro Martín", email = "alvaromrtn@gmail.com")),
+					servers = {		@Server(description = "Production server", url = "http://54.174.126.218:8000"),
+									@Server(description = "Development server", url = "http://localhost:8080")})
 public class Application {
     
     public static void main(String[] args) {
--- rest-user-service-presentation/src/main/java/com/axpe/exercices/presentation/controllers/EmployeeController.java
+++ rest-user-service-presentation/src/main/java/com/axpe/exercices/presentation/controllers/EmployeeController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.axpe.exercices.presentation.http.CustomHeaders;
 import com.axpe.exercices.service.dto.EmployeeAddDTO;
 import com.axpe.exercices.service.dto.EmployeeDTO;
@@ -15,28 +17,34 @@ import com.axpe.exercices.service.dto.EmployeeDTO;
 public interface EmployeeController {
 
 	@RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
+	@ResponseBody
 	ResponseEntity<?> getEmployee(@PathVariable(value = "id", required = true) long id,
 			@RequestHeader(value = CustomHeaders.X_REQUEST_ID, required = true) UUID xRequestID);
 
 	@RequestMapping(value = "/employee/{id}", method = RequestMethod.PATCH)
+	@ResponseBody
 	ResponseEntity<?> updateEmployee(@PathVariable(value = "id", required = true) long id,
 			@RequestHeader(value = CustomHeaders.X_REQUEST_ID, required = true) UUID xRequestID,
 			@RequestBody EmployeeDTO employeeDTO);
 
 	@RequestMapping(value = "/employee/{id}", method = RequestMethod.DELETE)
+	@ResponseBody
 	ResponseEntity<?> deleteEmployee(@PathVariable(value = "id", required = true) long id,
 			@RequestHeader(value = CustomHeaders.X_REQUEST_ID, required = true) UUID xRequestID);
 
 	@RequestMapping(value = "/employee", method = RequestMethod.POST)
+	@ResponseBody
 	ResponseEntity<?> addEmployee(@RequestHeader(value = CustomHeaders.X_REQUEST_ID, required = true) UUID xRequestID,
 			@RequestBody EmployeeAddDTO employeeDTO);
 
 	@RequestMapping(value = "/employees", method = RequestMethod.GET)
+	@ResponseBody
 	ResponseEntity<?> getAllEmployees(
 			@RequestHeader(value = CustomHeaders.X_REQUEST_ID, required = true) UUID xRequestID);
 
 	// BORRAR:
 	@RequestMapping(value = "/employeesTodo", method = RequestMethod.GET)
+	@ResponseBody
 	ResponseEntity<?> getAllEmployeesTodo(
 			@RequestHeader(value = CustomHeaders.X_REQUEST_ID, required = true) UUID xRequestID);
 
